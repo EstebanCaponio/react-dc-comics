7007feee31fdc37361f305aff11fe593443161c9
EstebanCaponio <estebancaponio@gmail.com>
2025-04-03 18:33:26 +0200

    feat: main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,11 @@
 function Main() {
-    return (<main>main</main>);
+    return (<main>
+        <a href="#">
+            <i class="fa-solid fa-arrow-right"></i>
+            Content goes here
+            <i class="fa-solid fa-arrow-left"></i>
+        </a>
+    </main>);
 }
 
 export default Main;
