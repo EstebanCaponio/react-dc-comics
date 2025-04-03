c9208640da7aea5a6c4e5e2b734bfffe30b3f554
EstebanCaponio <estebancaponio@gmail.com>
2025-04-03 20:05:19 +0200

    feat: footer center

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import merchandise from "../assets/img/buy-comics-merchandise.png";
 import subscription from "../assets/img/buy-comics-subscriptions.png";
 import shop from "../assets/img/buy-comics-shop-locator.png";
 import visa from "../assets/img/buy-dc-power-visa.svg";
+import dcLogoBig from "../assets/img/dc-logo-bg.png";
 
 function Footer() {
     return (<footer>
@@ -52,7 +53,56 @@ function Footer() {
             </ul>
 
         </div>
-        <div></div>
+        <div className="footer-center">
+            <div className="container">
+                <div className="container-links">
+                    <div>
+                        <ul>
+                            <li className="list-title">DC COMICS</li>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                            <li className="list-title list-title2">SHOP</li>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li className="list-title">DC</li>
+                            <li>Terms Of Use</li>
+                            <li>Privacy Policy</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshop</li>
+                            <li>CPSC Certificates</li>
+                            <li>Rating</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li className="list-title">SITES</li>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <img src={dcLogoBig} alt="" />
+                </div>
+            </div>
+        </div>
         <div></div>
 
     </footer>);
