c6ccccafa3eb7f28a0edc2103d6acfb38ead5d60
EstebanCaponio <estebancaponio@gmail.com>
2025-04-03 17:57:54 +0200

    feat: scaffolding

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Logo from "./Logo";
+
+function Header() {
+    return (
+        <>
+            <header className="container-main">
+                <Logo />
+            </header>
+        </>
+    );
+}
+
+export default Header;
