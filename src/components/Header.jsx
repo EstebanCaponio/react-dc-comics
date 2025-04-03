fed0ab3714429ffe0bc7566b6b8e732580a6e0eb
EstebanCaponio <estebancaponio@gmail.com>
2025-04-03 18:14:19 +0200

    feat: finish header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,20 @@ function Header() {
         <>
             <header className="container-main">
                 <Logo />
+                <nav>
+                    <ul className="ul-menu">
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                    </ul>
+                </nav>
             </header>
         </>
     );
