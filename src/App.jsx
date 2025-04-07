8b41d10ab11862ddb202ea79c05da9c0a356ffc7
EstebanCaponio <estebancaponio@gmail.com>
2025-04-07 18:27:53 +0200

    fix: fixed general errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Footer from "./components/footer"
+import Footer from "./components/Footer";
 import Header from "./components/header"
 import Jumbotron from "./components/jumbotron";
 import Main from "./components/main";
